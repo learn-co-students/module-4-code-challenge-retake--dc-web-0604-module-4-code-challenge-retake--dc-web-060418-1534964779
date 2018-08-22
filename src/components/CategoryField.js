cb10d004c44b09e5e638a2e026641c2ad977df6b
github username <github email address>
2018-08-22 16:25:23 -0400

    done

--- src/components/CategoryField.js
+++ src/components/CategoryField.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const CategoryField = (props) => {
-  const { category, checked } = props
+const CategoryField = props => {
+  const { category, checked, handleChange } = props;
 
   return (
     <div className=" four wide field">
       <div className="ui radio checkbox">
-
         <input
           type="radio"
           name="category"
-          checked={ checked }
+          checked={checked}
+          onChange={() => handleChange(category)}
         />
-        <label>{ category }</label>
-
+        <label>{category}</label>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryField
+export default CategoryField;
--- src/components/CategorySelector.js
+++ src/components/CategorySelector.js
@@ -1,34 +1,29 @@
-import React from 'react'
-import CategoryField from './CategoryField'
-import categories from '../categories'
+import React from "react";
+import CategoryField from "./CategoryField";
+import categories from "../categories";
 
-const CategorySelector = (props) => {
+const CategorySelector = ({ activeCategory, handleChange }) => {
+  console.log(activeCategory);
+  const categoryFields = categories.map((category, i) => {
+    const checked = category === activeCategory;
 
-  const categoryFields = categories.map(
-    (category, i) => {
-
-     const checked = category === props.activeCategory
-
-      return (
-        <CategoryField
-          key={ i }
-          checked={ checked }
-          category={ category }
-        />
-      )
-    }
-  )
-
+    return (
+      <CategoryField
+        key={i}
+        checked={checked}
+        category={category}
+        handleChange={handleChange}
+      />
+    );
+  });
 
   return (
     <div className="sixteen wide column">
       <div className="ui form">
-        <div className="inline fields">
-          { categoryFields }
-        </div>
+        <div className="inline fields">{categoryFields}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategorySelector
+export default CategorySelector;
